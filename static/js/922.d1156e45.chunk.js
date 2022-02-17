0a255a35c2d4cd0b5c6e72539264d4755d845a58
Maryna-Zakrevska <84293555+Maryna-Zakrevska@users.noreply.github.com>
2022-02-17 01:03:45 +0000

    Deploying to gh-pages from @ Maryna-Zakrevska/goit-react-hw-05-movies@4c45846e86141600503046dcfea9aab5e5ad70d7 🚀

---
+++ static/js/922.d1156e45.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[922],{922:function(t,n,e){e.r(n),e.d(n,{HomeListStyled:function(){return u},HomeListDivStyled:function(){return c}});var i,o,r=e(168),s=e(5751),u=s.ZP.ul(i||(i=(0,r.Z)(["\n  list-style: none;\n"]))),c=s.ZP.div(o||(o=(0,r.Z)(["\n  margin-top: 20px;\n  margin-bottom: 20px;\n"])))}}]);
+//# sourceMappingURL=922.d1156e45.chunk.js.map
